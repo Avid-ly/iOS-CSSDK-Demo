764cf0aae3c40e4345c20c9c56214a5fba4a89a8
steve <909476821@qq.com>
2021-11-24 11:29:40 +0800

    2.1.0.5

---
+++ CSSDKDemo/CSSDKDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  CSSDKDemo
+//
+//  Created by steve on 2021/11/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ CSSDKDemo/Pods/CSSDK/Framework/CSSDK/CServiceSDK.framework/Headers/CServiceSDKVersion.h
@@ -0,0 +1,14 @@
+//
+//  CServiceSDKVersion.h
+//  IosSdk
+//
+//  Created by steve on 2020/4/7.
+//  Copyright © 2020 customerservice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define CSSDKVERSION            @"2.1.0.5"
+#define CSSDK_SERVICE_VERSION   @"2105"
+#define CSSDK_BUILD             @"1"
+#define CSSDKVersionString      @"CSSDK_VERSION_2105.1"
